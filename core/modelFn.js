644e60e0c2a0a72c0c3cd7d4be499cf10583945b
beemarsh <anup8eguy@gmail.com>
2022-08-14 05:15:12 +0545

    Making DD

--- core/modelFn.js
+++ core/modelFn.js
@@ -1,5 +1,6 @@
 import {launchCamera} from 'react-native-image-picker';
 
+import {server} from './server';
 
 export async function onScanPress() {
   const result = await launchCamera({
@@ -9,5 +10,28 @@ export async function onScanPress() {
 
   console.log(result);
 
-  return result;
+  let our_data = result.assets[0];
+  let body = new FormData();
+
+  body.append('image', {
+    uri: our_data['uri'],
+    name: our_data['fileName'],
+    filename: our_data['fileName'],
+    type: our_data['type'],
+  });
+  body.append('Content-Type', 'image/png');
+
+  const request = await fetch(server.url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    body: body,
+  });
+
+  const json = await request.json();
+
+  console.log(json);
+
+  return result.assets[0];
 }
